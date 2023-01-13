d344c30a483ed007168aea1053c25ead3d589e18
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-12 22:02:23 -0700

    messing with the elements

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test!</Text>
+      <Text>Welcome to Done With It!</Text>
       <StatusBar style="auto" />
     </View>
   );
