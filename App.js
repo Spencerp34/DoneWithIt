f7b8a348372c86bc07e85802ede801ee92f432f6
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-15 12:18:51 -0700

    work in progress - photos

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
-  
-  console.log("App executed")
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Welcome to Done With It!</Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <Image source={{uri: "https://picsum.photos/200/300", width: 200, height: 200}} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center"
   },
 });
